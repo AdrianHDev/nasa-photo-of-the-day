3d6294d89e64902e32f0f6934b99fa169f8341f4
AdrianHDev <adrianhensondev@gmail.com>
2021-05-13 20:11:43 -0400

    Migrate Posts.css to styled-components

--- src/Components/Posts/Post.js
+++ src/Components/Posts/Post.js
@@ -1,4 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledPost = styled.div`
+    display: inline-block;
+    padding: 2rem;
+    vertical-align: top;
+    width: 80%;
+    text-align: center;
+`
+const StyledPostTitle = styled.h2`
+    font-size: 2rem;
+`
+
+const StyledPostVideo = styled.iframe`
+    width:45vw;
+    height:50vh;
+    margin-bottom: 50px;
+`
+
+const StyledPostImg = styled.img`
+    height: 50vh;
+    margin-bottom:50px;
+`
+
+
 
 const Post = props => {
     console.log(props);
@@ -6,22 +31,24 @@ const Post = props => {
 
     if (posts[curPost] !== undefined ) {
         if (posts[curPost].media_type === 'image') {return (
-    <div className='post'>
-        <img src={posts[curPost].hdurl} alt="" title={`APOD Image: ${posts[curPost].title}`}></img>
-        <h2><a href={posts[curPost].hdurl}>{posts[curPost].title}</a></h2>
-        <b>{posts[curPost].date}</b>
-        <p>{posts[curPost].explanation}</p>
-    </div>)}
-        else if (posts[curPost].media_type === 'video' ){
-            const ytID = posts[curPost].url.split('/')[4];
-            console.log(posts[curPost].url)
-            return (
-            <div className='post'>
-                <iframe title={posts[curPost]}frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src={`https://www.youtube.com/embed/${ytID}?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0`}></iframe>
-                <h2><a href={posts[curPost].hdurl}>{posts[curPost].title}</a></h2>
+            <StyledPost>
+                <StyledPostImg src={posts[curPost].hdurl} alt="" title={`APOD Image: ${posts[curPost].title}`}></StyledPostImg>
+                <StyledPostTitle><a href={posts[curPost].hdurl}>{posts[curPost].title}</a></StyledPostTitle>
                 <b>{posts[curPost].date}</b>
                 <p>{posts[curPost].explanation}</p>
-            </div>)}
+            </StyledPost>)}
+        else if (posts[curPost].media_type === 'video' ){
+            
+            const ytID = posts[curPost].url.split('/')[4];
+            console.log(posts[curPost].url)
+
+            return (
+            <StyledPost>
+                <StyledPostVideo title={posts[curPost]}frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src={`https://www.youtube.com/embed/${ytID}?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0`}></StyledPostVideo>
+                <StyledPostTitle><a href={posts[curPost].hdurl}>{posts[curPost].title}</a></StyledPostTitle>
+                <b>{posts[curPost].date}</b>
+                <p>{posts[curPost].explanation}</p>
+            </StyledPost>)}
         }    
     return null;
 }
--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,6 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import Post from './Post';
 import './Posts.css';
+
+const StyledPosts = styled.div`
+    max-height: 85vh;
+    margin: 0 0.25vw 0 0.25vw;
+    padding: 5px 0.25vw 0 0.25vw;
+    overflow: auto;
+    background-color: cadetblue;
+`
+
+const StyledPostTitle = styled.h2`
+    font-size: 1.35rem;
+    border: 3px red solid;
+    padding: 10px;
+    margin-top: -3px;
+    background-color: darkturquoise;
+`
+
+const StyledPostMenu = styled.div`
+    width: 15%;
+    margin-right: 5%;
+    display: inline-block;
+    vertical-align:middle;
+`
 
 const Posts = (props) => {
     const { posts, curPost, setCurPost } = props;
@@ -8,16 +32,16 @@ const Posts = (props) => {
 
     console.log(posts, curPost);
     return (
-        <div className="posts">
-            <div className="posts-selector">    
+        <StyledPosts>
+            <StyledPostMenu>    
                 {posts.map((post, index) => {
-                    return(<h2 onClick={() => {
+                    return(<StyledPostTitle onClick={() => {
                         setCurPost(index);
-                    }} key={post.title}>{post.title}</h2>);
+                    }} key={post.title}>{post.title}</StyledPostTitle>);
                 })}
-            </div>
-        <Post posts={posts} curPost={curPost}></Post>
-        </div>
+            </StyledPostMenu>
+            <Post posts={posts} curPost={curPost}></Post>
+        </StyledPosts>
     )
 }
 export default Posts;
